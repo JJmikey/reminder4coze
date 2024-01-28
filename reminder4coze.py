c8eabdf7090475a33d030242463cf2eeaeb8abd4
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-01-28 10:15:37 +0800

    Update reminder4coze.py

--- reminder4coze.py
+++ reminder4coze.py
@@ -126,8 +126,9 @@ def manage_tasks():
                 'reminder_time': reminder_time.isoformat()  # Store as string in ISO format
             })
             ref.child("current_task_id").set(current_task_id)
+            send_reminder(task_details['task']) #test webhook message function
             return jsonify({'message': 'Task added', 'id': current_task_id}), 201
-            send_reminder(task_details['task'])
+            
         else:
             return jsonify({'message': 'Task is required'}), 400
 
